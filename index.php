7fdda329313047f1019627fd16ddfefd1b3a6756
DkNOCO <dereksqldba@gmail.com>
2018-10-18 17:57:44 -0600

    V1.03
    Moved CSS to a seperate page
    
    Added functionality:
    -Remediate type MW
    -View All Resources
    -View Resource Detail
    -View All Jobs
    -Logic for AdHoc Scan (still not working)
    -Added getResourceGroupChildren() to methods

--- index.php
+++ index.php
@@ -22,70 +22,7 @@ if (isset($_GET['uuid'])){
     <title>My Company Application</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        /* Style the header */
-        header {
-            background-color: #666;
-            padding: 10px;
-            text-align: center;
-            font-size: 35px;
-            color: white;
-        }
-
-        /* Create two columns/boxes that floats next to each other */
-        nav {
-            float: left;
-            width: 30%;
-            height: 400px; /* only for demonstration, should be removed */
-            background: #ccc;
-            padding: 20px;
-        }
-
-        /* Style the list inside the menu */
-        nav ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        article {
-            float: left;
-            padding: 20px;
-            width: 70%;
-            background-color: #f1f1f1;
-
-        }
-
-        /* Clear floats after the columns */
-        section:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
-        /* Style the footer */
-        footer {
-            background-color: #777;
-            padding: 10px;
-            text-align: center;
-            color: white;
-        }
-
-        /* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-        @media (max-width: 600px) {
-            nav, article {
-                width: 100%;
-                height: auto;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="base.css">
 </head>
 <body>
 
@@ -99,7 +36,13 @@ if (isset($_GET['uuid'])){
     <nav>
         <ul>
             <li><a href="index.php">Home</a><br></li>
-            <li><a href="index.php?view=AllResourceGroups">View all DCA Resource Groups</a><br></li>
+            <li><b>Resources:</b></li>
+            <li><a href="index.php?view=AllResourceGroups">Resource Groups</a><br></li>
+            <li><a href="index.php?view=AllResources">Resources</a><br></li>
+            <li><b>Jobs:</b></li>
+            <li><a href="index.php?view=ViewAllJobs">View All</a></li>
+            <li>View Running</li>
+            <li>View Scheduled</li>
 
         </ul>
     </nav>
@@ -119,6 +62,65 @@ if (isset($_GET['uuid'])){
                     }
                     echo "</table>";
                     break;
+
+                case "AllResources":
+                    echo "
+                        <table border=1 width=90%>
+                        <tr><td><b>Resource Name</b></td><td><b>Resource Type</b></td><td><b>Action</b></td></tr>
+                        ";
+                    foreach (getResource() as $resource){
+                        echo "<tr><td>".$resource['name']."</td><td>". $resource['resourceType'] ."</td><td><a href='index.php?view=Resource&uuid=".$resource['uuid']."'>View</a></td></tr>";
+                    }
+                    echo "</table>";
+                    break;
+
+                case "Resource":
+                    $resource = getResource($uuid,null, 'fields=ParentRG,PolicySubscription');
+                    echo "<h2>".$resource['name']."</h2><br>";
+                    echo "<h3>Resource Details:</h3>";
+                    echo "<b>Resource type:</b> ". $resource['resourceType']."<br>";
+                    echo "<b>Compliance Status</b> ".$resource['complianceStatus']."<br>";
+                    foreach ($resource['attributes'] as $attribute){
+                        echo "<b>".$attribute['name']."</b> ".$attribute['value']."<br>";
+                    }
+                    echo "<br><br><h2>Actions:</h2>";
+
+                    if($resource['policysubscription']['count']>0){
+                        foreach ($resource['policysubscription']['members'] as $member) {
+                            echo $member['policyName'] . " | <a href='index.php?view=AdhocJob&uuid=".$uuid . "&policyid=".$member['policyId']."'>Scan</a><br>";
+                        }
+                    }
+
+                    break;
+                case "AdhocJob":
+                    // start job
+                    // redirect to jobs
+                    //this doesnt work.....
+                    $data = adhocScan($uuid,$_GET['policyid']);
+                    var_dump($data);
+                    die();
+
+
+                    break;
+                case "ViewAllJobs":
+
+                    $data = viewAllJobs();
+                    echo "<table border=1><tr><td><b>Job Type</b></td><td><b>Status</b></td><td><b>Start</b></td><td><b>End</b></td></tr>";
+                    if ($data['count']>0){
+                        foreach ($data['members'] as $member){
+                            echo "<tr><td>".$member['jobName']."</td>" .
+                                "<td>".$member['jobRequest']['jobCreationStatus']."</td>".
+                                "<td>".$member['jobStartTime']."</td>".
+                                "<td>".$member['jobEndTime']."</td>"
+                            ;
+                            echo "</tr>";
+
+                        }
+                    }
+                    echo "</table>";
+
+                    break;
+
                 case "ResourceGroup":
 
                     $RGP = getResourceGroupDetails($uuid);
@@ -148,13 +150,19 @@ if (isset($_GET['uuid'])){
                 case "AddMW":
 
                     echo"
+                        **Note there are many more options for Maint. Windows, only Monthly shown.**<br>
                         <form name='addmw' method='POST' action='index.php?view=CreateMW'>
                         <input type='hidden' name='rguuid' value='".$uuid."'>
                         <label>Name</label><input type='text' name='name'><br>
-                        <label>Window Type</label><select name='type'><option value='MAINTENANCE_WINDOW_TYPE_READ'>SCAN ONLY</option></select>
-                        <label>Recurrence Period</label><select name='period'><option value='MONTHS'>MONTHLY</option></select>
+                        <label>Window Type</label><select name='type'>
+                        <option value='MAINTENANCE_WINDOW_TYPE_READ'>Scan Only</option>
+                        <option value='MAINTENANCE_WINDOW_TYPE_READ_WRITE'>Scan and Remediate</option>
+                        </select>
+                        <label>Recurrence Period</label><select name='period'>
+                        <option value='MONTHS'>MONTHLY</option>
+                        </select><br>
                         <label>Recurrence Interval</label><input type='text' name='interval' value='2'><br>
-                        <label>Day of Month</label><input type='text' name='day'><br>
+                        <label>Day of Month To scan</label><input type='text' name='day'><br>
                         <label>Start Time</label><input type='text' name='starttime'><br>
                         <label>End Time</label><input type='text' name='endtime'><br>
                         <input type='submit' value='Add' name='Create MW'>
--- methods.php
+++ methods.php
@@ -8,7 +8,7 @@
 
 include_once "config.php";
 
-function callAPI($method,  $APIurl, $payload_JSON=null){
+function callAPI($method, $APIurl, $payload_JSON=null){
 global $dcaFQDN;
     if (isset($_SESSION['LAST_ACTIVITY']) && isset($_SESSION['DCATOKEN'])){
         if ((time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
@@ -60,12 +60,59 @@ function getResourceGroups($uuid=null)
         return $data['members'];
     }
 }
+
+function getResourceGrpChildren($uuid){
+    $data = callAPI( 'GET',"/urest/v1/resource_group/".$uuid."/child_resource", null);
+    return $data;
+}
+
 function getResourceGroupDetails($uuid){
     $data = callAPI( 'GET',"/urest/v1/resource_group/".$uuid."?fields=PolicySubscription,ChildResource,MaintenanceWindow", null);
     return $data;
 }
 
+function getResource($uuid=null, $payload=null, $fields=null){
+    $apiURL = "/urest/v1/resource";
+    if($uuid!=null && $fields==null){
+        $apiURL = $apiURL . "/" . $uuid;
+    }
+    elseif($uuid!=null && $fields!=null){
+        $apiURL = $apiURL . "/" . $uuid."?".$fields;
+    }
 
+    $data = callAPI('GET', $apiURL, $payload);
+
+    if ($uuid !== null) {
+        return $data;
+    } else {
+        return $data['members'];
+    }
+
+}
+
+function adhocScan($uuid, $policyId){
+
+    $payload = array(
+      "type" => "COMPLIANCE_SCAN",
+        "target" => array(
+          "type" => "RESOURCE",
+          "uuid" => $uuid
+        ),
+        "properties" => array(array(
+            "property" => "policyId",
+            "value" => $policyId
+        ))
+    );
+    $payload = json_encode($payload);
+    $data = callAPI("POST", "/urest/v1/scheduler/job_request",$payload);
+    return $data;
+}
+
+function viewAllJobs(){
+
+    $data = callAPI("GET", "/urest/v1/scheduler/job",null);
+    return $data;
+}
 
 ?>
 
