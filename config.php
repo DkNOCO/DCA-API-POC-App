3694fe0cbbaf5364b3beb52f7310b477b8cbafcd
DkNOCO <dereksqldba@gmail.com>
2019-02-27 11:06:37 -0700

    repo sync
    Just making sure the latest version of app is live

--- config.php
+++ config.php
@@ -5,10 +5,8 @@
  * Date: 10/17/2018
  * Time: 5:45 PM
  */
-
 $dcauser = ""; // DCA UI Username
 $dcapw = ""; // DCA UI Password
 $transportUser = ""; // see readme on how to obtain
 $transportPw = ""; // See readme on how to obtain
 $dcaFQDN=""; // FQDN of DCA Master
-
